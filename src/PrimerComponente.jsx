c2147ea3e65715e148af47c6889b18e5d9c95938
aleramirezsj <aleramirezsj@gmail.com>
2023-07-25 11:24:50 -0300

    primer componente

--- src/PrimerComponente.jsx
+++ src/PrimerComponente.jsx
@@ -1,5 +1,13 @@
+const string = 'esto es un texto'
+const number = 123456
+const array = ['Curso de React', 'Youtube', 4, 100000]
+const boolean = true
+const funcion = ()=> 'string desde funcion'
+const objeto = { nombre: 'Curso de Javascript', duracion:4}
+const fecha = new Date()
+
 export const PrimerComponente = () => {
   return (
-    <h1>Hola mundo</h1>
+    <h1>{ JSON.stringify(objeto) }</h1>
   )
 }
