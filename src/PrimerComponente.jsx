4f564026901a94c789d9c8972ce995e241d0f36b
aleramirezsj <aleramirezsj@gmail.com>
2023-07-27 02:47:17 -0300

    probando props, reglas, tipos, valores por defecto, etc

--- src/PrimerComponente.jsx
+++ src/PrimerComponente.jsx
@@ -1,24 +1,21 @@
 import './PrimerComponente.css'
+import PropTypes from 'prop-types'
 
-const string = 'esto es un texto'
-const number = 123456
-const array = ['Curso de React', 'Youtube', 4, 100000]
-const boolean = true
-const funcion = () => 'string desde funcion'
-const objeto = { nombre: 'Curso de Javascript', duracion: 4 }
-const fecha = new Date()
-
-export const PrimerComponente = () => {
+export const PrimerComponente = ({ titulo, subtitulo }) => {
   return (
     <>
-      <h1> Variables en JSX</h1>
-      <h4>Variable string</h4><p>{string}</p>
-      <h4>Variable number</h4><p>{number}</p>
-      <h4>Variable array</h4><p>{array}</p>
-      <h4>Variable boolean</h4><p>{boolean}</p>
-      <h4>Variable function</h4><p>{funcion()}</p>
-      <h4>Variable objeto</h4><p>{JSON.stringify(objeto)}</p>
-      <h4>Variable fecha</h4><p>{JSON.stringify(fecha)}</p>
+      <h1>{titulo}</h1> 
+      <h2>{subtitulo}</h2>     
     </>
   )
+}
+
+PrimerComponente.PropTypes={
+  titulo: PropTypes.string.isRequired,
+  subtitulo: PropTypes.string.isRequired
+}
+
+PrimerComponente.defaultProps ={
+  titulo:"Conociendo las props de React-",
+  subtitulo:"para enviar información entre componentes-"
 }
